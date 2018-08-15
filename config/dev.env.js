d972a8a22995a339e49485e6ad5340354462adf4
BoBoooooo <17746714@qq.com>
2018-08-15 16:16:14 +0800

    更新API接口地址

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://156.11.1.118:8081/api"',
+  BASE_API: '"http://156.11.1.196:9098/api"',
 })
