6b745715c52bd05787d64dc70040858fe08bb0ae
BoBoooooo <17746714@qq.com>
2018-11-29 14:55:54 +0800

    .

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// in development env not use Lazy Loading,because Lazy Loading too many pages will cause webpack hot update too slow.so only in production use Lazy Loading
-
-/* layout */
 import Layout from '../views/layout/Layout'
 
 Vue.use(Router)
 
- /**
-  * icon : the icon show in the sidebar
-  * hidden : if `hidden:true` will not show in the sidebar
-  * redirect : if `redirect:noredirect` will not redirct in the levelbar
-  * noDropdown : if `noDropdown:true` will not has submenu in the sidebar
-  **/
+
 export const constantRouterMap = [
   { path: '/login', component: ()=>import('@/views/login/index'), hidden: true },
   { path: '/404', component: ()=>import('@/views/404'), hidden: true },
