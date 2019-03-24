916ad37f08ff0c0bc104c70f0eacca87851da1ff
BoBoooooo <17746714@qq.com>
2019-03-24 11:18:49 +0800

    [fix]{Staff}: delete code

--- src/views/SIX/Staff.vue
+++ src/views/SIX/Staff.vue
@@ -4,21 +4,12 @@
       tableName="six_staff"
       toolbarButton="add,clear,search"
       handleButton="edit,delete"
-      :remoteFunctinos="remoteFunctinos"
     ></CrudTable>
   </div>
 </template>
 <script>
 export default {
   name: 'Staff',
-  data() {
-    return {
-      remoteFunctinos: {
-        funcGetStaff() {
-          return this.crud('list', 'six_staff', { pageSize: 100 })
-        },
-      },
-    }
-  },
+
 };
 </script>
