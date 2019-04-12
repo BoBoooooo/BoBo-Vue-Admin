5b0561e379902b80e250ccf96eea6d6c61522626
BoBoooooo <17746714@qq.com>
2019-04-12 16:10:43 +0800

    [fix]{FilenameOption}: 修复一处bug

--- src/views/SIX/components/FilenameOption.vue
+++ src/views/SIX/components/FilenameOption.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="display:inline-block;">
     <!-- $t is vue-i18n global function to translate lang -->
-    <label class="radio-label" style="margin-left:50px;">文件名:</label>
+    <label class="radio-label" style="margin-left:50px;">文件名</label>
     <el-input v-model="filename" style="width:350px!important;" prefix-icon="el-icon-document" />
   </div>
 </template>
