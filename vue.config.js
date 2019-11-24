5c5adcc81161e6b2415f84572206c91bfc11829b
BoBoooooo <17746714@qq.com>
2019-11-24 11:36:02 +0800

    [modify]{proxy}: 修改music代理地址

--- vue.config.js
+++ vue.config.js
@@ -97,7 +97,7 @@ module.exports = {
         },
       },
       '/music': {
-        target: 'https://music.163.com/', // 设置你调用的接口域名和端口号 别忘了加http
+        target: 'https://music.163.com/weapi/v3/playlist/detail', // 设置你调用的接口域名和端口号 别忘了加http
         changeOrigin: true,
         pathRewrite: {
           '^/music': '', // 这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
