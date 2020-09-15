c029395e8fd52ff98ba2632c569ded76f656e469
BoBoooooo <17746714@qq.com>
2020-09-15 16:32:26 +0800

    fix(bug): 修复网络错误情况下axios bug

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -88,7 +88,7 @@ service.interceptors.response.use(
     // http状态码200以外的情况
     // 请检查网络链接或联系管理员
     const msg = '请检查网络链接或联系管理员。';
-    MessageBox.alert(`${error.response.data.message}，${msg}`, '网络异常', {
+    MessageBox.alert(`${(error.response && error.response.data.message) || error.message}，${msg}`, '网络异常', {
       confirmButtonText: '重试',
       type: 'warning',
     });
