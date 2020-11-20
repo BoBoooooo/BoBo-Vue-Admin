d8d37b29f37bb7f0543d75856959ad08617c85e2
BoBoooooo <17746714@qq.com>
2020-11-20 13:53:10 +0800

    perf: 新增types文件夹定义全局type

---
+++ src/types/vuex.d.ts
@@ -0,0 +1,10 @@
+interface SystemConfig {
+   passwordLength: string,
+   clientTimeOut: string,
+   initialPassword: string,
+   systemName: string,
+}
+
+interface SideBarState {
+  opened: 1 | 0
+}
