e582feecc9d78e76c42228c1b671b2081e2f9ea4
BoBoooooo <17746714@qq.com>
2020-12-18 17:43:11 +0800

    feat(Person): 新增员工管理模块

---
+++ src/views/person/Person.vue
@@ -0,0 +1,30 @@
+<!--
+ * @file: 员工管理
+ * @author: BoBo
+ * @copyright: BoBo
+ * @Date: 2020年12月18日16:54:49
+-->
+
+<template>
+  <div class="page-container">
+    <CrudTable
+      ref="table"
+      tableName="person"
+      tableTitle="员工管理"
+      fullHeight
+      orderCondition="timestamp desc"
+      :visibleList="{
+        btnDel: true,
+      }"
+    />
+  </div>
+</template>
+
+<script>
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({
+  name: 'Person',
+})
+export default class Person extends Vue {}
+</script>
