7d2b9c22c00ce3628df8d3450a2baaac15d38d2d
BoBoooooo <17746714@qq.com>
2021-02-04 15:58:32 +0800

    perf: person demo 更新

--- src/views/person/Person.vue
+++ src/views/person/Person.vue
@@ -20,9 +20,11 @@
       <template #columnFormatter="{row,prop}">
         <el-tag v-if="prop === 'jobno'">{{ row.jobno }}</el-tag>
         <span v-if="prop === 'personname'"><i class="el-icon el-icon-user" style="color: red"></i>{{ row.personname }}</span>
-        <el-avatar fit="fill" v-if="prop === 'avatar'" shape="square" :src="row.avatar">
-          <img src="@/assets/404.png" />
-        </el-avatar>
+        <el-image style="width: 50px; height: 50px" fit="fill" v-if="prop === 'avatar'" :src="getAvatarUrl(row)" :preview-src-list="[getAvatarUrl(row)]">
+          <div slot="error" style="height: 100%">
+            <div class="error"><i class="el-icon-picture-outline"></i></div>
+          </div>
+        </el-image>
       </template>
     </CrudTable>
   </div>
@@ -34,5 +36,21 @@ import { Vue, Component } from 'vue-property-decorator';
 @Component({
   name: 'Person',
 })
-export default class Person extends Vue {}
+export default class Person extends Vue {
+  getAvatarUrl(row) {
+    return this.API_URL + row.avatar;
+  }
+}
 </script>
+<style lang="scss" scoped>
+.error {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background: #f5f7fa;
+  color: #909399;
+  font-size: 30px;
+}
+</style>
