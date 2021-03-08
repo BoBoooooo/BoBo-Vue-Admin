4d3c73eeabb74f98e6d8afd5e085a514672adb44
BoBoooooo <17746714@qq.com>
2021-03-08 15:16:49 +0800

    fix: 修复角色设置无法设置菜单bug

--- src/views/system/Role.vue
+++ src/views/system/Role.vue
@@ -6,23 +6,46 @@
 -->
 <template>
   <div class="page-container">
-    <CrudTable ref="table"
-               tableName="role"
-               tableTitle="角色列表"
-               fullHeight
-               orderCondition="timestamp desc"
-               :visibleList="{
-                 btnDel:true,
-               }" />
+    <CrudTable
+      ref="table"
+      tableName="role"
+      tableTitle="角色列表"
+      fullHeight
+      :btnAddOnClick="btnAddOnClick"
+      :btnEditOnClick="btnEditOnClick"
+      orderCondition="timestamp desc"
+      :visibleList="{
+        btnDel: true,
+      }"
+    />
+      <!-- 角色编辑对话框 -->
+    <RoleDialog ref="dialog"
+                tableName="role"
+                @afterSave="roleDialogOnClose" />
   </div>
 </template>
 
 <script>
 import { Component, Vue, Watch } from 'vue-property-decorator';
+import RoleDialog from './components/RoleDialog.vue';
 
 @Component({
   name: 'Role',
+  components: {
+    RoleDialog,
+  },
 })
 export default class Role extends Vue {
+  btnAddOnClick() {
+    this.$refs.dialog.showDialog();
+  }
+
+  btnEditOnClick(row) {
+    this.$refs.dialog.showDialog({ id: row.id }, 1, row);
+  }
+
+  roleDialogOnClose() {
+    this.$refs.table.tableReload();
+  }
 }
 </script>
